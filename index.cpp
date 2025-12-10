486ea9629d4e259960b9c440c11d5713aea556e4
Divi202 <divya8217274@gmail.com>
2025-12-10 17:41:18 +0530

    first commit

---
+++ index.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+/* #include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;*/
+using namespace std;
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int a;
+    cin >> a;
+    cout << "Hello, World!" << a << std::endl;
+
+    return 0;
+}
