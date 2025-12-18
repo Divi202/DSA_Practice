0ce9625ff77fe2915ba7b42f400899e58ebac5af
Divi202 <divya8217274@gmail.com>
2025-12-18 22:01:46 +0530

    array

---
+++ firstcopy.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+/* #include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;*/
+using namespace std;
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+
+    cout << "Hello Again" << std::endl;
+
+    return 0;
+}
